f6aea0e5fbf5970a99864e6a87d1e9e3db2a71f7
Júlio Gomes <juliocgomes.aog@gmail.com>
2021-07-20 18:02:34 -0300

    Ajustes finais

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ include_once('conexao.php');
  * Iniciando o try catch.
  */
 try {
-
+    /**
+     * DESENVOLVER UM MENU
+     */
 } catch (Throwable | PDOException $e) {
     echo $e->getCode().'</br>';
     echo $e->getMessage();
